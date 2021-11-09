7923e2905a87cba46a918d17d195f7ce1511e349
Tiago Rodrigues <isatiago098@gmail.com>
2021-11-09 17:30:38 -0300

    resolucao exercicio doadordesangue

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace doadordesangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+             Console.Write("Por favor digite sua idade---");
+
+             idade = Convert.ToInt32(Console.ReadLine());
+
+             if (idade > 18 && idade < 67)
+             {
+                  Console.WriteLine(" voce pode doador de sangue");
+             }
+             
+
+             else
+             {
+                 Console.WriteLine("voce nao pode doar sangue");
+             }
+
+
+
+
+        }
+    }
+}
